6f46fc432d592800ed5e8efa73c83ca3e4fa2994
Sanghyeon <lsh1107@kaist.ac.kr>
2020-01-11 20:46:32 +0900

    Foxes can detect our rabbit

---
+++ 456/Assets/Scripts/Environment/Water.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public float waterDept;
+    public Transform playerTransform;
+    public GameObject waterShaderObject;
+
+    private float renderingDst;
+
+    private void Start()
+    {
+        renderingDst = Camera.main.farClipPlane;
+    }
+
+    public void Update()
+    {
+        waterShaderObject.transform.position = new Vector3(playerTransform.position.x, waterDept, playerTransform.position.z);
+    }
+}
